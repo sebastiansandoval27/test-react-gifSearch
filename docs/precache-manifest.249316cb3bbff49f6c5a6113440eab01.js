686d216544e5973a6b655e1f3d3860fc521d240d
unknown <juan.sandovals@usantoto.edu.co>
2020-07-21 10:47:50 -0500

    Docs Added

---
+++ docs/precache-manifest.249316cb3bbff49f6c5a6113440eab01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8b88627563657b14a50e8a9df60a273",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7d31f91e544266ab936",
+    "url": "/static/css/main.551edc09.chunk.css"
+  },
+  {
+    "revision": "07fb45130e48c7aab52d",
+    "url": "/static/js/2.8688a3cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8688a3cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7d31f91e544266ab936",
+    "url": "/static/js/main.c601a0ed.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
